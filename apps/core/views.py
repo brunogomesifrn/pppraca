d64f04077db694ccd0e5ef3c50dfec74c94d35e8
isacszc <isacszc@hotmail.com>
2025-07-14 16:50:48 -0300

    modularizei o layout do projeto

--- apps/core/views.py
+++ apps/core/views.py
@@ -6,3 +6,21 @@ def index(request):
 
 def acervo(request):
     return render(request, 'acervo.html')
+
+def acontecimentos(request):
+    return render(request, 'acontecimentos.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def espacos(request):
+    return render(request, 'espacos.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def sobrePraca(request):
+    return render(request, 'sobre-praca.html')
+
+def visitacao(request):
+    return render(request, 'visitacao.html')
--- pppraca/urls.py
+++ pppraca/urls.py
@@ -20,8 +20,12 @@ from apps.core.views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('acervo', acervo, name='acervo'),
-    
-
+    path('acervo/', acervo, name='acervo'),
+    path('acontecimentos/', acontecimentos, name="acontecimentos"),
+    path('contato/', contato, name='contato'),
+    path('espacos/', espacos, name='espacos'),
+    path('login/', login, name='login'),
+    path('sobre-a-praca/', sobrePraca, name='sobrePraca'),
+    path('visitacao/', visitacao, name='visitacao'),
     path('admin/', admin.site.urls),
 ]
